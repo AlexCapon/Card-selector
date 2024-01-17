d065f573aa22cfbc52e5b2054ed75cc35e021325
AlexCapon <125947007+AlexCapon@users.noreply.github.com>
2024-01-17 22:17:45 +0700

    Project placed into repository

---
+++ index.js
@@ -0,0 +1,53 @@
+const slides = document.querySelectorAll('.slide');
+const container = document.querySelector('.container');
+document.addEventListener('keydown', ({ key }) => keySelect(key));
+for (const slide of slides) {
+    slide.addEventListener('click', ({ target }) => clickSelect(target));
+}
+function clickSelect(target) {
+    if (target.classList.contains('active')) {
+        clearActiveClasses();
+    } else {
+        changeActiveClasses(target);
+    };
+}
+function keySelect(key) {
+    const isActive = (element) => element.classList.contains('active');
+    let activeIndex;
+    slides.forEach((slide, index) => { if (isActive(slide)) activeIndex = index });
+
+    if (activeIndex === undefined) {
+        if (key === 'ArrowRight') changeActiveClasses(slides[0]);
+        if (key === 'ArrowLeft') changeActiveClasses(slides[slides.length - 1]);
+    } else {
+        if (key === 'ArrowDown' || key === 'ArrowUp') clearActiveClasses();
+        if (key === 'ArrowRight') {
+            if (activeIndex === slides.length - 1) {
+                changeActiveClasses(slides[0]);
+            } else {
+                changeActiveClasses(slides[activeIndex + 1]);
+            }
+        }
+        if (key === 'ArrowLeft') {
+            if (activeIndex === 0) {
+                changeActiveClasses(slides[slides.length - 1]);
+            } else {
+                changeActiveClasses(slides[activeIndex - 1]);
+            }
+        }; 
+    }
+}
+function getActiveSlide(index, key) {
+}
+function changeActiveClasses(target) {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+        slide.classList.add('inactive')
+    })
+    target.classList.toggle('inactive')
+    target.classList.toggle('active')
+}
+function clearActiveClasses() {
+    slides.forEach((slide) => slide.classList.remove('inactive'));
+    slides.forEach((slide) => slide.classList.remove('active'));
+}
